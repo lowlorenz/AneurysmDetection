ed37469b7f7839102ddfc5b430b9662fa81c19ef
lorenz <lorenz.hufe@web.de>
2022-06-20 12:05:37 +0200

    format code

--- unet/metrics.py
+++ unet/metrics.py
@@ -3,7 +3,8 @@ from torch import nn
 from scipy import signal
 from scipy import spatial
 
-## https://towardsdatascience.com/dealing-with-class-imbalanced-image-datasets-1cbd17de76b5
+# https://towardsdatascience.com/dealing-with-class-imbalanced-image-datasets-1cbd17de76b5
+
 
 class Metric(nn.Module):
     def forward(self, prediction, label):
@@ -15,45 +16,50 @@ class Metric(nn.Module):
         tn = label.shape[0] - (torch.sum(label) + fp)
         return tp, tn, fp, fn
 
+
 class TverskyLoss(Metric):
-    
+
     def __init__(self, alpha):
         super().__init__()
         self.alpha = alpha
-    
+
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
-        tversky = tp /(tp + self.alpha * fn + (1-self.alpha) * fp)
+        tversky = tp / (tp + self.alpha * fn + (1-self.alpha) * fp)
         return 1 - tversky
-    
+
 
 class DiceLoss(Metric):
-    
+
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
-        dice =  2*tp/(2*tp + fn + fp)
+        dice = 2*tp/(2*tp + fn + fp)
         return 1 - dice
 
+
 class Sensitivity(Metric):
-    
+
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
-        return tp /(tp + fn)
+        return tp / (tp + fn)
+
 
 class Specificity(Metric):
 
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
-        return tn /(tn + fp)
-    
+        return tn / (tn + fp)
+
+
 class Accuracy(Metric):
 
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
         return (tn+tp)/(tp+tn+fp+fn)
-    
+
+
 class Jaccard(Metric):
-    
+
     def forward(self, prediction, label):
         tp, tn, fp, fn = super().forward(prediction, label)
         return (tp) / (tp + fp + fn)
@@ -61,31 +67,35 @@ class Jaccard(Metric):
 ##################### Segmentation metrics ##########################
 
 
-#We need flattened pred/labels for these metrics
-class SegmentationMetric(nn.Module):  
+# We need flattened pred/labels for these metrics
+class SegmentationMetric(nn.Module):
     def forward(self, prediction, label):
         prediction = prediction.reshape(-1)
         label = label.reshape(-1)
         return prediction, label
 
+
 class StdDev(SegmentationMetric):
-  
+
     def forward(self, prediction, label):
         pred, lab = super().forward(prediction, label)
         return (pred - lab).std()
-    
+
+
 class Bias(SegmentationMetric):
     def forward(self, prediction, label):
         pred, lab = super().forward(prediction, label)
-        #absolute difference
+        # absolute difference
         diff = torch.abs(torch.sum(pred - lab))
-        #amount of non-zero elements in label mask
+        # amount of non-zero elements in label mask
         sum_label = torch.sum(label)
         return 1/sum_label*diff
 
+
 class AvgDist(SegmentationMetric):
-  
+
     def forward(self, prediction, label):
         pred, lab = super().forward(prediction, label)
-        avg = spatial.distance.cdist(pred.numpy(), label.numpy(), metric='euclidean')
+        avg = spatial.distance.cdist(
+            pred.numpy(), label.numpy(), metric='euclidean')
         return sum(sum(avg)) / (avg.shape[0] * avg.shape[1])
